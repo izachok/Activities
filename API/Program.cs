a62e7a296fcaa690f479baeae64bf00a1d84fc29
Olga Tiutiunnyk <izachok@gmail.com>
2021-10-08 09:39:28 +0300

    add identity to server side

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
+using Domain;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,8 +26,9 @@ namespace API
 								try
 								{
 										var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+										var userManager = scope.ServiceProvider.GetRequiredService<UserManager<AppUser>>();
 										context.Database.Migrate();
-										await Seed.SeedData(context);
+										await Seed.SeedData(context, userManager);
 								}
 								catch (Exception ex)
 								{
